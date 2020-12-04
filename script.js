7b1c15612ff388b8095ab9020d84d317ad003315
takashi-okimoto <okimaru.03@icloud.com>
2020-12-04 19:12:12 +0900

    add logs

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 const damageRange = 0.3;
+let logIndex = 0;
 
 const playerData = {
   name: "taka",
   hp: 100,
   attack: 5,
-  deffence: 2
+  defence: 2
 }
 
-const enemyData = {
-  name: "obachan",
-  hp: 50,
-  attack: 4,
-  deffence: 1
-}
+const enemiesData = [
+  {
+    name: "おじさん",
+    hp: 50,
+    attack: 4,
+    defence: 1
+  },
+  {
+    name: "kids",
+    hp: 60,
+    attack: 5,
+    defence: 2
+  },
+  {
+    name: "メンヘラ",
+    hp: 30,
+    attack: 15,
+    defence: 1
+  },
+  {
+    name: "おばちゃん",
+    hp: 100,
+    attack: 5,
+    defence: 2
+  }
+];
+
+const enemyData = enemiesData[Math.floor(Math.random() * enemiesData.length)];
 
 playerData["maxHp"] = playerData["hp"]
 enemyData["maxHp"] = enemyData["hp"]
@@ -21,16 +44,24 @@ function insertText(id, text) {
   document.getElementById(id).textContent = text;
 }
 
-function damageCalculation(attack, deffence) {
+function damageCalculation(attack, defence) {
   const maxDamage = attack * (1 + damageRange),
         minDamage = attack * (1 - damageRange),
         attackDamage = Math.floor(Math.random() * (maxDamage - minDamage) + minDamage);
-  const damage = attackDamage - deffence;
+  const damage = attackDamage - defence;
   if (damage < 1) {
     return 0
   } else {
     return damage;
   }
+}
+
+function insertLog(texts) {
+  const logsElement = document.getElementById("logs"),
+        createLog = document.createElement("li");
+  logIndex ++;
+  createLog.innerHTML = logIndex + ": " + texts;
+  logsElement.insertBefore(createLog, logsElement.firstChild)
 }
 
 insertText("playerName", playerData["name"]);
@@ -42,39 +73,45 @@ insertText("curenntEnemyHp", enemyData["hp"]);
 insertText("maxEnamyHp", enemyData["hp"]);
 
 document.getElementById("attack").addEventListener("click", function() {
-  let endGame = false;
-  const playerDamage = damageCalculation(playerData["attack"], enemyData["deffence"]),
-        enemyDamage = damageCalculation(enemyData["attack"], playerData["deffence"]);
+  let victory = false;
+      defeat = false;
+
+  const playerName = '<span style="color: blue;">' + playerData["name"] + "</span>";
+        enemyName = '<span style="color: red;">' + enemyData["name"] + "</span>";
 
 
+  const playerDamage = damageCalculation(playerData["attack"], enemyData["defence"]);
   enemyData["hp"] -=  playerDamage;
-  playerData["hp"] -= enemyDamage;
-
   insertText("curenntEnemyHp", enemyData["hp"]);
-  insertText("curenntPlayerHp", playerData["hp"]);
-
   document.getElementById("curenntEnemyHpGaugeValue").style.width = (enemyData["hp"] / enemyData["maxHp"] * 100) + "%";
-  document.getElementById("curenntPlayerHpGaugeValue").style.width = (playerData["hp"] / playerData["maxHp"] * 100) + "%";;
+  insertLog(playerName + "の攻撃！" + enemyName + "に" + playerDamage + "のダメージ！");
 
   if (enemyData["hp"] <= 0) {
     alert("Win！");
-    endGame = true;
+    victory = true;
 
     enemyData["hp"] = 0;
     insertText("curenntEnemyHp", enemyData["hp"]);
     document.getElementById("curenntEnemyHpGaugeValue").style.width = "0%";
-
-
-  } else if (playerData["hp"] <= 0) {
-    alert("Lose...");
-    endGame = true;
-
-    playerData["hp"] = 0;
-    insertText("curenntEnemyHp", playerData["hp"]);
-    document.getElementById("curenntPlayerHpGaugeValue").style.width = "0%";
   }
 
-  if (endGame) {
+  if (!victory) {
+    const enemyDamage = damageCalculation(enemyData["attack"], playerData["defence"]);
+    playerData["hp"] -= enemyDamage;
+    insertText("curenntPlayerHp", playerData["hp"]);
+    document.getElementById("curenntPlayerHpGaugeValue").style.width = (playerData["hp"] / playerData["maxHp"] * 100) + "%";
+    insertLog(enemyName + "の攻撃！" + playerName + "に" + enemyDamage + "のダメージ！");
+
+    if (playerData["hp"] <= 0) {
+      alert("Lose...");
+      defeat = true;
+
+      playerData["hp"] = 0;
+      insertText("curenntEnemyHp", playerData["hp"]);
+      document.getElementById("curenntPlayerHpGaugeValue").style.width = "0%";
+    }
+  }
+  if (victory || defeat) {
     this.classList.add("deactive");
   }
 });
