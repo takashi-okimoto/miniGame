04cc77621fd929b107fbac764558295c81457339
takashi-okimoto <okimaru.03@icloud.com>
2020-12-04 10:49:06 +0900

    js add random damage range

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+const damageRange = 0.3;
+
 const playerData = {
   name: "taka",
-  hp: 100
+  hp: 100,
+  attack: 5,
+  deffence: 2
 }
 
 const enemyData = {
   name: "obachan",
-  hp: 50
+  hp: 50,
+  attack: 4,
+  deffence: 1
 }
 
 function insertText(id, text) {
   document.getElementById(id).textContent = text;
+}
+
+function damageCalculation(attack, deffence) {
+  const maxDamage = attack * (1 + damageRange),
+        minDamage = attack * (1 - damageRange),
+        attackDamage = Math.floor(Math.random() * (maxDamage - minDamage) + minDamage);
+  const damage = attackDamage - deffence;
+  if (damage < 1) {
+    return 0
+  } else {
+    return damage;
+  }
 }
 
 insertText("playerName", playerData["name"]);
@@ -22,9 +40,12 @@ insertText("maxEnamyHp", enemyData["hp"]);
 
 document.getElementById("attack").addEventListener("click", function() {
   let endGame = false;
+  const playerDamage = damageCalculation(playerData["attack"], enemyData["deffence"]),
+        enemyDamage = damageCalculation(enemyData["attack"], playerData["deffence"]);
 
-  enemyData["hp"] -=  10;
-  playerData["hp"] -= 10;
+
+  enemyData["hp"] -=  playerDamage;
+  playerData["hp"] -= enemyDamage;
 
   insertText("curenntEnemyHp", enemyData["hp"]);
   insertText("curenntPlayerHp", playerData["hp"]);
@@ -32,9 +53,16 @@ document.getElementById("attack").addEventListener("click", function() {
   if (enemyData["hp"] <= 0) {
     alert("Win！");
     endGame = true;
+
+    enemyData["hp"] = 0;
+    insertText("curenntEnemyHp", enemyData["hp"]);
+
   } else if (playerData["hp"] <= 0) {
     alert("Lose...");
     endGame = true;
+
+    playerData["hp"] = 0;
+    insertText("curenntEnemyHp", playerData["hp"]);
   }
 
   if (endGame) {
