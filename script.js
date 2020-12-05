b9fb7ce8a919243eee4ecc7cb58117769827fb12
takashi-okimoto <okimaru.03@icloud.com>
2020-12-05 09:02:35 +0900

    add mask modal

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const damageRange = 0.3;
+const damageRange = 0.3,
       criticalHitRate = 0.1;
-let logIndex = 0;
+let logIndex = 0,
+    nowKilledNumber = 0,
+    targetKillNumber = 2;
 
 const playerData = {
-  name: "taka",
+  name: "勇者Takahiko",
   hp: 100,
   attack: 5,
   defence: 2
@@ -13,30 +15,32 @@ const enemiesData = [
   {
     name: "サラリーマン",
     hp: 50,
-    attack: 4,
+    attack: 3,
     defence: 1
   },
   {
     name: "少年A",
     hp: 60,
-    attack: 5,
+    attack: 4,
     defence: 1
   },
   {
     name: "カノジョ",
     hp: 30,
-    attack: 15,
+    attack: 10,
     defence: 1
   },
   {
     name: "近所のおばちゃん",
-    hp: 100,
-    attack: 5,
+    hp: 80,
+    attack: 4,
     defence: 2
   }
 ];
-
-const enemyData = enemiesData[Math.floor(Math.random() * enemiesData.length)];
+for (let i = 0; i < enemiesData.length; i++) {
+  enemiesData[i]["maxHp"] = enemiesData[i]["hp"];
+}
+let enemyData = enemiesData[Math.floor(Math.random() * enemiesData.length)];
 
 playerData["maxHp"] = playerData["hp"]
 enemyData["maxHp"] = enemyData["hp"]
@@ -65,6 +69,15 @@ function insertLog(texts) {
   logsElement.insertBefore(createLog, logsElement.firstChild)
 }
 
+function showModal(title, hiddenNextButton = false) {
+  document.getElementById("mask").classList.add("active");
+  document.getElementById("modal").classList.add("active");
+  document.getElementById("modalTitle").textContent = title;
+  if (hiddenNextButton) {
+    document.getElementById("modalNextButton").classList.add("hidden");
+  }
+}
+
 insertText("playerName", playerData["name"]);
 insertText("curenntPlayerHp", playerData["hp"]);
 insertText("maxPlayerHp", playerData["hp"]);
@@ -72,6 +85,9 @@ insertText("maxPlayerHp", playerData["hp"]);
 insertText("enemyName", enemyData["name"]);
 insertText("curenntEnemyHp", enemyData["hp"]);
 insertText("maxEnamyHp", enemyData["hp"]);
+
+insertText("nowKilledNumber", nowKilledNumber);
+insertText("targetKillsNumber", targetKillNumber);
 
 document.getElementById("attack").addEventListener("click", function() {
   let victory = false;
@@ -92,12 +108,13 @@ document.getElementById("attack").addEventListener("click", function() {
   document.getElementById("curenntEnemyHpGaugeValue").style.width = (enemyData["hp"] / enemyData["maxHp"] * 100) + "%";
 
   if (enemyData["hp"] <= 0) {
-    alert("Win！");
     victory = true;
 
     enemyData["hp"] = 0;
     insertText("curenntEnemyHp", enemyData["hp"]);
     document.getElementById("curenntEnemyHpGaugeValue").style.width = "0%";
+
+    showModal(enemyData["name"] + "を倒した！");
   }
 
   if (!victory) {
@@ -114,15 +131,39 @@ document.getElementById("attack").addEventListener("click", function() {
     document.getElementById("curenntPlayerHpGaugeValue").style.width = (playerData["hp"] / playerData["maxHp"] * 100) + "%";
 
     if (playerData["hp"] <= 0) {
-      alert("Lose...");
       defeat = true;
 
       playerData["hp"] = 0;
       insertText("curenntEnemyHp", playerData["hp"]);
       document.getElementById("curenntPlayerHpGaugeValue").style.width = "0%";
+
+      showModal(enemyData["name"] + "に負けた...", true);
     }
   }
   if (victory || defeat) {
     this.classList.add("deactive");
   }
+
+  if (victory) {
+    nowKilledNumber++;
+    insertText("nowKilledNumber", nowKilledNumber);
+
+    if (nowKilledNumber === targetKillNumber) {
+      showModal("Congratulaaation!!", true);
+    }
+  }
+});
+
+document.getElementById("modalNextButton").addEventListener("click", function() {
+  enemyData["hp"] = enemyData["maxHp"]
+  enemyData = enemiesData[Math.floor(Math.random() * enemiesData.length)];
+  insertText("enemyName", enemyData["name"]);
+  insertText("curenntEnemyHp", enemyData["hp"]);
+  insertText("maxEnamyHp", enemyData["hp"]);
+  document.getElementById("curenntEnemyHpGaugeValue").style.width = "100%";
+
+
+  document.getElementById("mask").classList.remove("active");
+  document.getElementById("modal").classList.remove("active");
+  document.getElementById("attack").classList.remove("deactive");
 });
